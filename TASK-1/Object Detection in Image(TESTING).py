fa566c3928a893328a2059c39df35f7428e48cfc
Denim Thangjam <65865531+Denim-Thangjam@users.noreply.github.com>
2021-07-08 20:45:40 +0530

    Add files via upload

---
+++ TASK-1/Object Detection in Image(TESTING).py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Made by : Thangjam Denim Singh
+
+# In[1]:
+
+
+import cv2
+import numpy as np
+
+
+# In[2]:
+
+
+# Transfering the weights and cfg from yolov3_416
+# Using the coco.names as labels
+
+net = cv2.dnn.readNet('yolov3.weights', 'yolov3.cfg')
+classes = []
+with open('coco.names','r') as f:
+    classes = f.read().splitlines()
+    
+print(classes)
+
+
+# ## Image
+
+# In[23]:
+
+
+## Reading the image
+
+img = cv2.imread('Trial4.jpg')
+height, width, _ = img.shape
+
+## Preparing the image
+
+blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+net.setInput(blob)
+output_layers_names = net.getUnconnectedOutLayersNames()
+layerOutputs = net.forward(output_layers_names)
+
+boxes=[]
+confidences = []
+class_ids = []
+
+## Visualization
+## extracts all info from the layer's output
+for output in layerOutputs:
+    ## extracts info from each of the outputs
+    for detection in output:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            
+            ## Finding the coordinates of the center, and measurement of width and height of box.
+            ## As we had normalized the values,
+            ## so we need to multiply it with original size to get the correct coordinates
+            center_x = int(detection[0]*width)
+            center_y = int(detection[1]*height)
+            w = int(detection[2]*width)
+            h = int(detection[3]*height)
+            
+            ## Yolo detects the center of the boxes,
+            ## So in order to work with OpenCV, we need to find the top left corner dims
+            x = int(center_x - w/2)
+            y = int(center_y - h/2)
+            
+            boxes.append([x, y, w, h])
+            confidences.append((float(confidence)))
+            class_ids.append(class_id)
+            
+
+# print(len(boxes))
+## To remove the overlap of boxes on a single object, we'll use the Non Maximum Supression
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+# print(indexes.flatten())
+
+font = cv2.FONT_HERSHEY_PLAIN
+colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+
+## Loop to identify each of the object detected
+for i in indexes.flatten():
+    x, y, w, h = boxes[i]
+    label = str(classes[class_ids[i]])
+    confidence = str(round(confidences[i], 2))
+    color = colors[i]
+    cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+    cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255,255,255), 2)
+
+## Viewing the image
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+img = cv2.imread('Trial4.jpg')
+height, width, _ = img.shape
+
+blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+net.setInput(blob)
+output_layers_names = net.getUnconnectedOutLayersNames()
+layerOutputs = net.forward(output_layers_names)
+
+boxes=[]
+confidences = []
+class_ids = []
+
+for output in layerOutputs:
+    for detection in output:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            
+            center_x = int(detection[0]*width)
+            center_y = int(detection[1]*height)
+            w = int(detection[2]*width)
+            h = int(detection[3]*height)
+            
+            x = int(center_x - w/2)
+            y = int(center_y - h/2)
+            
+            boxes.append([x, y, w, h])
+            confidences.append((float(confidence)))
+            class_ids.append(class_id)
+            
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+
+font = cv2.FONT_HERSHEY_PLAIN
+colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+
+for i in indexes.flatten():
+    x, y, w, h = boxes[i]
+    label = str(classes[class_ids[i]])
+    confidence = str(round(confidences[i], 2))
+    color = colors[i]
+    cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+    cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255,255,255), 2)
+
+cv2.imshow('Image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+# ## Video (Accurate but very slow)
+
+# 
+# cap = cv2.VideoCapture('screen-capture.mp4')
+# 
+# while True:
+#     
+#     _, img = cap.read()
+#     height, width, _ = img.shape
+# 
+#     blob = cv2.dnn.blobFromImage(img, 1/255, (416, 416), (0, 0, 0), swapRB=True, crop=False)
+#     net.setInput(blob)
+#     output_layers_names = net.getUnconnectedOutLayersNames()
+#     layerOutputs = net.forward(output_layers_names)
+# 
+#     boxes=[]
+#     confidences = []
+#     class_ids = []
+# 
+#     for output in layerOutputs:
+#         for detection in output:
+#             scores = detection[5:]
+#             class_id = np.argmax(scores)
+#             confidence = scores[class_id]
+#             if confidence > 0.5:
+# 
+#                 center_x = int(detection[0]*width)
+#                 center_y = int(detection[1]*height)
+#                 w = int(detection[2]*width)
+#                 h = int(detection[3]*height)
+# 
+#                 x = int(center_x - w/2)
+#                 y = int(center_y - h/2)
+# 
+#                 boxes.append([x, y, w, h])
+#                 confidences.append((float(confidence)))
+#                 class_ids.append(class_id)
+# 
+#     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+# 
+#     font = cv2.FONT_HERSHEY_PLAIN
+#     colors = np.random.uniform(0, 255, size=(len(boxes), 3))
+#     
+#     if len(indexes)>0:
+#         for i in indexes.flatten():
+#             x, y, w, h = boxes[i]
+#             label = str(classes[class_ids[i]])
+#             confidence = str(round(confidences[i], 2))
+#             color = colors[i]
+#             cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+#             cv2.putText(img, label + " " + confidence, (x, y+20), font, 2, (255,255,255), 2)
+# 
+#     cv2.imshow('Image', img)
+#     key = cv2.waitKey(1)
+#     if key==27:
+#         break
+#     
+# cap.release()
+# cv2.destroyAllWindows()
+
+# In[ ]:
+
+
+
+
